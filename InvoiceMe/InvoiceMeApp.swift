253efc5b8f60b19680ad5e45b9b6f1164e1ff3e2
Rajan Maharjan <admin@astraltechnology.com.au>
2023-10-31 15:34:46 +1100

    Initial Commit

---
+++ InvoiceMe/InvoiceMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoiceMeApp.swift
+//  InvoiceMe
+//
+//  Created by Rajan Maharjan on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct InvoiceMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
