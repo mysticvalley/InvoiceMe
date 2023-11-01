10a9c5b9a5f424a9c88c5d8c59ee59cd915e16c8
Rajan Maharjan <admin@astraltechnology.com.au>
2023-11-01 15:59:55 +1100

    [XERO] Adds invoice model and mocks

---
+++ InvoiceMe/Models/Invoice+Mocks.swift
@@ -0,0 +1,67 @@
+//
+//  Invoice+Mocks.swift
+//  InvoiceMe
+//
+//  Created by Rajan Maharjan on 31/10/2023.
+//
+
+import Foundation
+
+public extension Invoice {
+    static var mockSingleItem: Invoice {
+        .init(
+            number: 1,
+            date: .mock,
+            lineItems: [
+                .mockOne
+            ]
+        )
+    }
+    
+    static var mockMultipleItem: Invoice {
+        .init(
+            number: 2,
+            date: .mock,
+            lineItems: [
+                .mockOne, .mockTwo, .mockThree
+            ]
+        )
+    }
+}
+
+public extension InvoiceLine {
+    static var mockOne: InvoiceLine {
+        .init(
+            lineId: 1,
+            description: "line 1",
+            quantity: 10,
+            cost: 50.2
+        )
+    }
+    
+    static var mockTwo: InvoiceLine {
+        .init(
+            lineId: 2,
+            description: "line 2",
+            quantity: 15,
+            cost: 35.8
+        )
+    }
+    
+    static var mockThree: InvoiceLine {
+        .init(
+            lineId: 3,
+            description: "line 3",
+            quantity: 23,
+            cost: 23.3
+        )
+    }
+}
+
+public extension Date {
+    static var mock: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/YYYY"
+        return dateFormatter.date(from: "10/10/2023")!
+    }
+}
---
+++ InvoiceMe/Models/Invoice.swift
@@ -0,0 +1,91 @@
+//
+//  Invoice.swift
+//  InvoiceMe
+//
+//  Created by Rajan Maharjan on 31/10/2023.
+//
+
+import Foundation
+
+public struct Invoice {
+    let number: Int
+    let date: Date
+    var lineItems: [InvoiceLine]
+    
+    public init(number: Int, date: Date, lineItems: [InvoiceLine] = []) {
+        self.number = number
+        self.date = date
+        self.lineItems = lineItems
+    }
+    
+    mutating func addLine(line: InvoiceLine) {
+        lineItems.append(line)
+    }
+    
+    mutating func removeLine(lineId: Int) {
+        guard let itemToRemoveIndex = lineItems.firstIndex(where: { $0.lineId == lineId }) else {
+            print("line item not found with lineId")
+            return
+        }
+        
+        let removedItem = lineItems.remove(at: itemToRemoveIndex)
+        print("Removed item from line: \(removedItem)")
+    }
+    
+    /// GetTotal should return the sum of (Cost * Quantity) for each line item
+    func getTotal() -> Double {
+                
+        let total = lineItems.reduce(0) { partialResult, line in
+            return partialResult + line.cost * Double(line.quantity)
+        }
+        
+        return total
+    }
+}
+
+extension Invoice {
+    /// MergeInvoices appends the items from the sourceInvoice to the current invoice
+    func mergeInvoice(sourceInvoice: Invoice) -> Invoice {
+        .init(number: 30, date: Date())
+    }
+    
+    /// Creates a deep clone of the current invoice (all fields and properties)
+    func cloneInvoice() -> Invoice {
+        .init(number: 20, date: Date())
+    }
+    
+    /// order the lineItems by Id
+    func orderLineItems() {
+        fatalError("not implemented")
+    }
+    
+    /// returns the number of the line items specified in the variable `max`
+    func previewLineItems(_ max: Int) -> [InvoiceLine] {
+        fatalError("not implemented")
+    }
+    
+    /// remove the line items in the current invoice that are also in the sourceInvoice
+    func removeItems(from sourceInvoice: Invoice) {
+        fatalError("not implemented")
+    }
+    
+    /// Outputs string containing the following (replace [] with actual values):
+    /// Invoice Number: [InvoiceNumber], InvoiceDate: [DD/MM/YYYY], LineItemCount: [Number of items in LineItems]
+    func toString() -> String {
+        fatalError("not implemented")
+    }
+}
+
+public struct InvoiceLine {
+    let lineId: Int
+    let description: String
+    let quantity: Int
+    let cost: Double
+    
+    public init(lineId: Int, description: String, quantity: Int, cost: Double) {
+        self.lineId = lineId
+        self.description = description
+        self.quantity = quantity
+        self.cost = cost
+    }
+}
